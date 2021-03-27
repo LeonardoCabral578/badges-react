053b3a31584b0e6628e86945cad9a0bc2c46ba9f
Leo <leo.cabral1016@gmail.com>
2021-03-27 17:01:44 -0300

    form

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,15 +2,59 @@ import React, { Component } from "react";
 
 import "./styles/BadgeNew.css";
 import header from "../images/badge-header.svg";
+
 import Navbar from "../components/Navbar";
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends Component {
+  state = {
+    form: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      jobTitle: "",
+      twitter: "",
+    },
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
   render() {
     return (
       <div>
         <Navbar />
         <div className="BadgeNew__hero">
           <img className="img-fluid" src={header} alt="logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName={this.state.form.firstName}
+                lastName={this.state.form.lastName}
+                twitter={this.state.form.twitter}
+                jobTitle={this.state.form.jobTitle}
+                email={this.state.form.email}
+                avatarUrl="https://es.gravatar.com/userimage/203973929/1d22c4bedf7d56fb40689dfc39a983a6.jpg?size=200"
+              />
+            </div>
+
+            <div className="col-6">
+              <BadgeForm
+                onChange={this.handleChange}
+                formValues={this.state.form}
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
